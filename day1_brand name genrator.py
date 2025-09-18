188942250a88fd2f3b93ac115cc992fdc99bbfa8
ankitsh79038-ship-it <ankitsh79038@gmail.com>
2025-09-18 20:52:51 +0530

    my python code

---
+++ day1_brand name genrator.py
@@ -0,0 +1,33 @@
+# print("Welcome to Brand Name Generator Game !!!")
+
+# # city_name=input("Enter The Where You Born :  ")
+# # pet_name=input("Enter The Name Of Your Pet :  ")
+
+# # Brand_Name=city_name +" "+ pet_name
+
+# # print(f"Your Band Name Should be {Brand_Name}")
+
+
+# # Another Way To Do The Code  
+# print(f"Your Brand Name Should be {input("Enter Your Born City: ")+" "+input("Enter Your Pet Name: ")}")
+
+#fix brand name game 
+
+# print("hello welcome to the brand name generator game!")
+# brand_name = 'vivo phone is a very good product in market'
+# print(brand_name)
+
+#user brand name game 
+# print("Hello welcome to the brand game name generator")
+# brand_name =input("enter any brand name")
+# pet_name =input("entre any brand name")
+
+# print(brand_name+ " "+pet_name)
+
+#
+print("Hello welcome to the brand game name generator")
+ex_name=input("entre your ex name")
+gf_name=input("enter your gf name")
+
+wife_name=ex_name +" "+ gf_name
+print(f"your wife name should be {wife_name}")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+# print("hello welcome to the brand name generator game!")
+# brand_name = 'vivo phone is a very good product in market'
+# print(brand_name)
