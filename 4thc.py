7b09d2023647d7820a4ec8db0de82718735afd44
ankitsh79038-ship-it <ankitsh79038@gmail.com>
2026-02-03 20:04:44 +0530

    pratice python

--- 4thc.py
+++ 4thc.py
@@ -6,4 +6,18 @@ states_of_matter[1] = "florida"
 
 # states_of_matter.append("pennsylvania")
 states_of_matter.extend(["pennsylvania", "arizona", "alaska"])
+states_of_matter.remove("california")                           
 print(states_of_matter)
+line1 = [" ", " ", " "]
+line2 = [" ", " ", " "]
+line3 = [" ", " ", " "]
+map = [line1, line2, line3]
+print("hiding your treasure X marks the spot.")
+position = input() # where do you want to put the treasure?
+#your code below
+letter = position[0].lower()
+abc = ["a", "b", "c"]
+letter_index = abc.index(letter)
+number_index = int(position[1]) - 1
+map[number_index][letter_index] = "X"
+
